2d6de332cfbe92e45676f6c846017e411164a57d
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2020-03-19 11:57:14 +0100

    Add model for actual data

---
+++ src/main/java/com/example/covid19api/model/Actual.java
@@ -0,0 +1,46 @@
+package com.example.covid19api.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Actual {
+
+    private int confirmed;
+
+    private int deaths;
+
+    private int recovered;
+
+    public Actual() {
+    }
+
+    public Actual(int confirmed, int deaths, int recovered) {
+        this.confirmed = confirmed;
+        this.deaths = deaths;
+        this.recovered = recovered;
+    }
+
+    public int getConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(int confirmed) {
+        this.confirmed = confirmed;
+    }
+
+    public int getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(int deaths) {
+        this.deaths = deaths;
+    }
+
+    public int getRecovered() {
+        return recovered;
+    }
+
+    public void setRecovered(int recovered) {
+        this.recovered = recovered;
+    }
+}
