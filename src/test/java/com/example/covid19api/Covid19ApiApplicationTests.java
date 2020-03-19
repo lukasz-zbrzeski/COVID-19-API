0e562ac06b324755390082412a709a8dc90f6026
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2020-03-19 11:30:33 +0100

    Initial commit

---
+++ src/test/java/com/example/covid19api/Covid19ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covid19api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
