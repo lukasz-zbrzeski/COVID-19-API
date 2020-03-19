886ba2cb6bbd036c943fa8bebf914259df8e5857
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2020-03-19 16:38:30 +0100

    Update controller - add new endpoints

--- src/main/java/com/example/covid19api/controller/DataController.java
+++ src/main/java/com/example/covid19api/controller/DataController.java
@@ -1,17 +1,38 @@
 package com.example.covid19api.controller;
 
-import com.example.covid19api.parsers.ActualParser;
+import com.example.covid19api.service.DataService;
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
 public class DataController {
-    ActualParser actualParser = new ActualParser();
+    private Gson gson = new Gson();
+
+    private DataService dataService = new DataService();
 
     @GetMapping("/actual")
     public String actual() {
-        return actualParser.parseData();
+        return dataService.getActualData();
+    }
+
+    @GetMapping("/location")
+    public String searchDataByLocation(@RequestParam(required = false) String country, @RequestParam(required = false) String region) {
+        if (country != null && region != null) {
+            return this.dataService.searchDataByCountryAndRegion(country, region);
+        } else if (country != null) {
+            return this.dataService.searchDataByCountry(country);
+        } else if (region != null) {
+            return this.dataService.searchDataByRegion(region);
+        } else {
+            HashMap<String, String> errorMessage = new HashMap<>();
+            errorMessage.put("message", "Data not found!");
+            return gson.toJson(errorMessage);
+        }
     }
 }
