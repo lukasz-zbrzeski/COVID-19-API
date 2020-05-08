10a276428429253577dc9a9fb2d166e4f601eba3
lukasz-zbrzeski <lukasz.zbrzeski1@gmail.com>
2020-05-08 18:03:42 +0200

    Inject strategy context and update return statement of methods

--- src/main/java/com/example/covid19api/controller/DataController.java
+++ src/main/java/com/example/covid19api/controller/DataController.java
@@ -1,20 +1,23 @@
 package com.example.covid19api.controller;
 
-import com.example.covid19api.parsers.strategy.ActualParserStrategy;
-import com.example.covid19api.parsers.strategy.HistoryParserStrategy;
-import com.example.covid19api.parsers.strategy.LocationParserStrategy;
-import com.example.covid19api.parsers.strategy.ParserContext;
+import com.example.covid19api.parsers.strategy.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class DataController {
-    private final ParserContext context = new ParserContext();
+    private final ParserContext context;
+
+    @Autowired
+    public DataController(ParserContext context) {
+        this.context = context;
+    }
 
     @RequestMapping(value = "/actual", method = RequestMethod.GET)
     @ResponseBody
     public String searchActualData() {
-        return context.getData(null, null, null, null, new ActualParserStrategy());
+        return context.getData(null, null, null, null, context.findStrategy(StrategyName.ACTUAL_PARSER_STRATEGY));
     }
 
     @RequestMapping(value = "/actual/location", method = RequestMethod.GET)
@@ -24,12 +27,12 @@ public class DataController {
             @RequestParam(defaultValue = "") String region,
             @RequestParam(defaultValue = "") String city
     ) {
-        return context.getData(country, region, city, null, new LocationParserStrategy());
+        return context.getData(country, region, city, null, context.findStrategy(StrategyName.LOCATION_PARSER_STRATEGY));
     }
 
     @RequestMapping(value = "/history/{date}", method = RequestMethod.GET)
     @ResponseBody
     public String searchHistoryData(@PathVariable String date) {
-        return context.getData(null, null, null, date, new HistoryParserStrategy());
+        return context.getData(null, null, null, date, context.findStrategy(StrategyName.HISTORY_PARSER_STRATEGY));
     }
 }
